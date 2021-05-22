431318d9ef1be8b5bdff150c58dd6daf36bf614e
saravana-ganesh <saravanaganesh@protonmail.com>
2021-05-22 22:55:15 +0530

    OTP verifications

--- Middleware/Covid19Dashboard/src/main/java/com/covid/delete/TestDelete.java
+++ Middleware/Covid19Dashboard/src/main/java/com/covid/delete/TestDelete.java
@@ -1,11 +1,18 @@
 package com.covid.delete;
 
 public class TestDelete {
-	
+	int i;
+	static {
+		System.out.println("Static block");
+	}
+	TestDelete(){
+		System.out.println(i);
+	}
 	public static void main(String[] args) throws CloneNotSupportedException {
-		Student s1 = new Student(1,"saravana");
+		/*Student s1 = new Student(1,"saravana");
 		Student s2 = (Student)s1.clone();
-		System.out.println(s1==s2);
+		System.out.println(s1==s2);	*/	
+		new TestDelete();
 	}
 
 }
